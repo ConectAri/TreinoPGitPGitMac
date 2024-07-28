ed18a372bacc9c386dd24b874f148b5f6a04836d
ConectAri <105406479+ConectAri@users.noreply.github.com>
2024-07-28 19:55:42 -0300

    Add files via upload

---
+++ TreinoPGitP/guardaItemPaulo/src/main/java/com/guardaItemPaulo/guardaItemPaulo/GuardaItemPauloApplication.java
@@ -0,0 +1,13 @@
+package com.guardaItemPaulo.guardaItemPaulo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuardaItemPauloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuardaItemPauloApplication.class, args);
+	}
+
+}
---
+++ TreinoPGitP/guardaItemPaulo/src/test/java/com/guardaItemPaulo/guardaItemPaulo/GuardaItemPauloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guardaItemPaulo.guardaItemPaulo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuardaItemPauloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
